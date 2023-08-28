76e6cddccb1f6d86f05678027b44c32d6480a35b
MewanthaB <mewantha.b@incentivio.com>
2023-08-28 18:07:25 +0530

    Added library

--- vars/argocdUploadImage.groovy
+++ vars/argocdUploadImage.groovy
@@ -1,9 +1,15 @@
 def call(Map config = [:]) {
+    if (${config.GIT_BRANCH} == "main") {
+        ECR_IMAGE="${config.DOCKER_IMAGE_REPO}:${config.DOCKER_IMAGE_REPO_TAG}-v_${config.GIT_BRANCH}_${config.BUILD_NUMBER}"
+    } else {
+        ECR_IMAGE="${config.DOCKER_IMAGE_REPO_TAG}-v_${config.BRANCH}_${config.BUILD_NUMBER}"
+    }
+    
     sh """
         rm -rf deploy/kube-manifests-qa-argo
         mkdir deploy/kube-manifests-qa-argo
         cp -a deploy/kube-manifests-qa/. deploy/kube-manifests-qa-argo/
-        sed -i -e "s/\\(${config.DOCKER_IMAGE_REPO}:\\).*/\\1${config.ECR_IMAGE}/" deploy/kube-manifests-qa-argo/Deployment.yml
+        sed -i -e "s/\\(${config.DOCKER_IMAGE_REPO}:\\).*/\\1${ECR_IMAGE}/" deploy/kube-manifests-qa-argo/Deployment.yml
         git checkout -b "${config.GIT_BRANCH}"
         git pull origin "${config.GIT_BRANCH}"
         git branch
