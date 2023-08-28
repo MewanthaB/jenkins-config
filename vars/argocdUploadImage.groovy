985de106d3dc727ed4eac9874d7653d4cfc954db
MewanthaB <mewantha.b@incentivio.com>
2023-08-28 17:24:45 +0530

    Added library

--- vars/argocdUploadImage.groovy
+++ vars/argocdUploadImage.groovy
@@ -1,16 +1,16 @@
-def call(String DOCKER_IMAGE_REPO, String ECR_IMAGE, String GIT_BRANCH, String BUILD_NUMBER, String BRANCH) {
+def call(Map config = [:]) {
     sh """
         rm -rf deploy/kube-manifests-qa-argo
         mkdir deploy/kube-manifests-qa-argo
         cp -a deploy/kube-manifests-qa/. deploy/kube-manifests-qa-argo/
-        sed -i -e "s/\\(${DOCKER_IMAGE_REPO}:\\).*/\\1${ECR_IMAGE}/" deploy/kube-manifests-qa-argo/Deployment.yml
-        git checkout -b "${GIT_BRANCH}"
-        git pull origin "${GIT_BRANCH}"
+        sed -i -e "s/\\(${config.DOCKER_IMAGE_REPO}:\\).*/\\1${config.ECR_IMAGE}/" deploy/kube-manifests-qa-argo/Deployment.yml
+        git checkout -b "${config.GIT_BRANCH}"
+        git pull origin "${config.GIT_BRANCH}"
         git branch
         git add deploy/kube-manifests-qa-argo/Deployment.yml deploy/kube-manifests-qa-argo/Service.yml
         git commit -m "CI: Update image for ArgoCD"
-        git tag ${BRANCH}_${BUILD_NUMBER}
-        git push -u origin "${GIT_BRANCH}"
-        git push origin ${BRANCH}_${BUILD_NUMBER}
+        git tag ${config.BRANCH}_${config.BUILD_NUMBER}
+        git push -u origin "${config.GIT_BRANCH}"
+        git push origin ${config.BRANCH}_${config.BUILD_NUMBER}
     """
 }
