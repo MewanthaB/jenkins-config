dc5dfdb76513acba43be6d21ae30923580eb901f
MewanthaB <mewantha.b@incentivio.com>
2023-09-01 13:51:14 +0530

    Change build result

--- vars/argocdUploadImage.groovy
+++ vars/argocdUploadImage.groovy
@@ -7,13 +7,13 @@ def call(Map config = [:]) {
 
     try {
         sh """
+            git checkout -b "${config.GIT_BRANCH}"
+            git pull origin "${config.GIT_BRANCH}"
+            git branch
             rm -rf deploy/kube-manifests-qa-argo
             mkdir deploy/kube-manifests-qa-argo
             cp -a deploy/kube-manifests-qa/. deploy/kube-manifests-qa-argo/
             sed -i -e "s/\\(${config.DOCKER_IMAGE_REPO}:\\).*/\\1${ECR_IMAGE}/" deploy/kube-manifests-qa-argo/Deployment.yml
-            git checkout -b "${config.GIT_BRANCH}"
-            git pull origin "${config.GIT_BRANCH}"
-            git branch
             git add deploy/kube-manifests-qa-argo/Deployment.yml deploy/kube-manifests-qa-argo/Service.yml
             git commit -m "[AUTOMATION] Sync deployment file with new image version"
             git tag ${config.BRANCH}_${config.BUILD_NUMBER}
