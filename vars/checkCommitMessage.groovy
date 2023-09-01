8c4649ddc9752198f79ab72d6d11ecc653f7b1f1
MewanthaB <mewantha.b@incentivio.com>
2023-09-01 12:42:35 +0530

    Change build result

--- vars/checkCommitMessage.groovy
+++ vars/checkCommitMessage.groovy
@@ -2,6 +2,6 @@ def call() {
     def commitMessage = sh(script: 'git log -1 --pretty=%B', returnStdout: true).trim()
     if (commitMessage == '[AUTOMATION] Sync deployment file with new image version') {
         currentBuild.result = 'SUCCESS'
-        return
+        sh("exit 0")
     }
 }
