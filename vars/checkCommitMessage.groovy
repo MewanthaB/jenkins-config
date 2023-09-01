e0c132de9316346de85eeb606d7f6e62915abb2e
MewanthaB <mewantha.b@incentivio.com>
2023-09-01 12:22:44 +0530

    Change build result

--- vars/checkCommitMessage.groovy
+++ vars/checkCommitMessage.groovy
@@ -2,6 +2,5 @@ def call() {
     def commitMessage = sh(script: 'git log -1 --pretty=%B', returnStdout: true).trim()
     if (commitMessage == '[AUTOMATION] Sync deployment file with new image version') {
         currentBuild.result = 'SUCCESS'
-        error('Pipeline execution skipped due to specific commit message')
     }
 }
