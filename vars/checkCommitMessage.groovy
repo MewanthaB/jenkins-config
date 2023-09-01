cfb3eaf9ed345dcdb70bc22e318e0cfae7ef77f3
MewanthaB <mewantha.b@incentivio.com>
2023-09-01 15:32:01 +0530

    Change build result

--- vars/checkCommitMessage.groovy
+++ vars/checkCommitMessage.groovy
@@ -1,8 +1,7 @@
 def call() {
     def commitMessage = sh(script: 'git log -1 --pretty=%B', returnStdout: true).trim()
     if (commitMessage == '[AUTOMATION] Sync deployment file with new image version') {
-        //currentBuild.result = 'ABORTED'
-        currentBuild.rawBuild.@result = hudson.model.Result.SUCCESS
+        currentBuild.result = 'ABORTED'
         warnError('Pipeline execution skipped due to specific commit message')
     }
 }
