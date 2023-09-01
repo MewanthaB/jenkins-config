0bbb10bdb71303182660a67278d7aba40dbd90d8
MewanthaB <mewantha.b@incentivio.com>
2023-09-01 13:39:45 +0530

    Change build result

--- vars/argocdUploadImage.groovy
+++ vars/argocdUploadImage.groovy
@@ -5,18 +5,22 @@ def call(Map config = [:]) {
         ECR_IMAGE="${config.DOCKER_IMAGE_REPO_TAG}-v_${config.BRANCH}_${config.BUILD_NUMBER}"
     }
 
-    sh """
-        rm -rf deploy/kube-manifests-qa-argo
-        mkdir deploy/kube-manifests-qa-argo
-        cp -a deploy/kube-manifests-qa/. deploy/kube-manifests-qa-argo/
-        sed -i -e "s/\\(${config.DOCKER_IMAGE_REPO}:\\).*/\\1${ECR_IMAGE}/" deploy/kube-manifests-qa-argo/Deployment.yml
-        git checkout -b "${config.GIT_BRANCH}"
-        git pull origin "${config.GIT_BRANCH}"
-        git branch
-        git add deploy/kube-manifests-qa-argo/Deployment.yml deploy/kube-manifests-qa-argo/Service.yml
-        git commit -m "[AUTOMATION] Sync deployment file with new image version"
-        git tag ${config.BRANCH}_${config.BUILD_NUMBER}
-        git push -u origin "${config.GIT_BRANCH}"
-        git push origin ${config.BRANCH}_${config.BUILD_NUMBER}
-    """
+    try {
+        sh """
+            rm -rf deploy/kube-manifests-qa-argo
+            mkdir deploy/kube-manifests-qa-argo
+            cp -a deploy/kube-manifests-qa/. deploy/kube-manifests-qa-argo/
+            sed -i -e "s/\\(${config.DOCKER_IMAGE_REPO}:\\).*/\\1${ECR_IMAGE}/" deploy/kube-manifests-qa-argo/Deployment.yml
+            git checkout -b "${config.GIT_BRANCH}"
+            git pull origin "${config.GIT_BRANCH}"
+            git branch
+            git add deploy/kube-manifests-qa-argo/Deployment.yml deploy/kube-manifests-qa-argo/Service.yml
+            git commit -m "[AUTOMATION] Sync deployment file with new image version"
+            git tag ${config.BRANCH}_${config.BUILD_NUMBER}
+            git push -u origin "${config.GIT_BRANCH}"
+            git push origin ${config.BRANCH}_${config.BUILD_NUMBER}
+        """
+    } catch (Exception e) {
+        println("Something went wrong with Git operation for ArgoCD")
+    }
 }
